be81f5fed2af5281fc5aea0d2a7ba503f242961b
GGGarcia09 <106259326+GGGarcia09@users.noreply.github.com>
2024-05-17 12:53:26 -0600

    socket nuevoindex.js

--- index.js
+++ index.js
@@ -1,60 +1,28 @@
-let port = process.env.PORT || 5000;
+const http = require('http');
 
-let IO = require("socket.io")(port, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+const server = http.createServer();
+
+const io = require('socket.io')(server, {
+    cors: { origin: '*' }
 });
 
-IO.use((socket, next) => {
-  if (socket.handshake.query) {
-    let callerId = socket.handshake.query.callerId;
-    socket.user = callerId;
-    next();
-  }
+io.on('connection', (socket) => {
+    console.log('Se ha conectado un cliente');
+    socket.on('chat_message', (data) => {
+        io.emit('chat_message', data);
+        
+    });
+    socket.on('subscribe', function (room) {
+        socket.join(room);
+    });
+    socket.on('send message', function (data) {
+        socket.broadcast.to(data.room).emit('conversation private post', { message: data.message });
+
+    });
 });
 
-IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
-  socket.join(socket.user);
+server.listen(3000);
 
-  socket.on("makeCall", (data) => {
-    let calleeId = data.calleeId;
-    let sdpOffer = data.sdpOffer;
 
-    socket.to(calleeId).emit("newCall", {
-      callerId: socket.user,
-      sdpOffer: sdpOffer,
-    });
-  });
-
-  socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
-
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
-    });
-  });
-
-  socket.on("IceCandidate", (data) => {
-    let calleeId = data.calleeId;
-    let iceCandidate = data.iceCandidate;
-
-    socket.to(calleeId).emit("IceCandidate", {
-      sender: socket.user,
-      iceCandidate: iceCandidate,
-    });
-  });
-
-  // Crear una nueva lista y agregar los identificadores de usuario
-  let connectedUsers = [];
-  IO.sockets.sockets.forEach((userSocket) => {
-    connectedUsers.push(userSocket.user);
-  });
-
-  // Emitir la lista de usuarios conectados a todos los clientes
-  IO.emit("userList", connectedUsers);
-});
+// instalar - npm isntall -g nodemon
+/// npm start
